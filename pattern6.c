0aa11859bd54cc4c0045db39388a61c28341a2c0
aaryantyagi182 <42247648+aaryantyagi182@users.noreply.github.com>
2018-10-09 23:26:30 +0530

    Add files via upload

---
+++ pattern6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k;
+    for(i=1;i<=10;i++)
+    {
+        for(j=i;j<=3;j++)
+            printf(" ");
+        for(k=1;k==2i-1;k++)
+        {
+         printf("%d", i);
+        }
+    }
+return 0;
+}
